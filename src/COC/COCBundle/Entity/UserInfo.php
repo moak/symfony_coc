929abef580d50636c8484b145d1bcbf6aaf235b2
Seyaa <barbot.max@gmail.com>
2015-01-15 02:45:20 +0100

    forgot one file

--- src/COC/COCBundle/Entity/UserInfo.php
+++ src/COC/COCBundle/Entity/UserInfo.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * UserInfo
  *
- * @ORM\Table(name="userInfo")
+ * @ORM\Table(name="userinfo")
  * @ORM\Entity(repositoryClass="COC\COCBundle\Repository\UserInfoRepository")
  */
 class UserInfo
