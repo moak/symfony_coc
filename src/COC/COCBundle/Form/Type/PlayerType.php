96f8be026f973ee2d3e0798fbb8ec19758f81c31
Seyaa <barbot.max@gmail.com>
2015-01-20 19:30:52 +0100

    sdfsdfsdfsdf

--- src/COC/COCBundle/Form/Type/PlayerType.php
+++ src/COC/COCBundle/Form/Type/PlayerType.php
@@ -25,7 +25,7 @@ class PlayerType extends AbstractType
             ->add('canon4' ,  'choice', array('choices' => array(0, 1, 2, 3, 4,5, 6, 7, 8, 9,10)))
             ->add('canon5' ,  'choice', array('choices' => array(0, 1, 2, 3, 4,5, 6, 7, 8, 9,10)))
             ->add('canon6' ,  'choice', array('choices' => array(0, 1, 2, 3, 4,5, 6, 7, 8, 9,10)))
-            ->add('canon7' ,  'choice', array('choices' => array(0, 1, 2, 3, 4,5, 6, 7, 8, 9,10)))
+
 
             ->add('mortar1' , 'choice', array('choices' => array(0, 1, 2, 3, 4, 5, 6, 7)))
             ->add('mortar2' , 'choice', array('choices' => array(0, 1, 2, 3, 4, 5, 6, 7)))
