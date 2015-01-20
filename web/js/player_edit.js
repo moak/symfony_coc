244be94d8a3bff3e95ee09a3ef31113db9baa58b
Florent Bejina <florent@bejina.fr>
2015-01-20 17:29:10 +0100

    player_edit V0.1

---
+++ web/js/player_edit.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+	var town_hall_level_max = 10;
+	var town_hall_level = $("#players_hall_town" ).val();
+	town_hall_level++;
+	alert(town_hall_level);
+	for (i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
+		$.each($("[data-town-hall-level-min=" + i + "]"), function() {
+			if( town_hall_level >= i ) $("[data-town-hall-level-min=" + i + "]").show();
+			else $("[data-town-hall-level-min=" + i + "]").hide();
+		});
+	}
+});
+	
+$( "#players_hall_town" ).on( "change", function() {
+	var town_hall_level_max = 10;
+	var town_hall_level = $("#players_hall_town" ).val();
+	town_hall_level++;
+	alert(town_hall_level);
+	for (i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
+		$.each($("[data-town-hall-level-min=" + i + "]"), function() {
+			if( town_hall_level >= i ) $("[data-town-hall-level-min=" + i + "]").show();
+			else $("[data-town-hall-level-min=" + i + "]").hide();
+		});
+	}			
+});
