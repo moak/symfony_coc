c2c7124e1f070c177546e99adbeae1025759601f
Florent Bejina <florent@bejina.fr>
2015-01-27 09:39:57 +0100

    Classement improvements (DataTables plugin) expand + sortable

--- web/js/players.js
+++ web/js/players.js
@@ -1,11 +1,45 @@
+var myArray = []; // tableau qui contiendra le HTML des row à expand
 
-$( "body" ).on( "click", ".glyphicon-plus", function() {	
-	//$('.glyphicon-minus').addClass('glyphicon-plus').removeClass('glyphicon-minus');
-	$(this).removeClass('glyphicon-plus').addClass('glyphicon-minus');
-	//$(".expendables").addClass("hidden");
-	$("#" + $(this).data("name")).removeClass('hidden');
+$( document ).ready(function(){
+
+	// récupération des row à stocker
+	$( ".expendables" ).each(function( index ) {
+		myArray[$(this).attr('id')] = $(this).html();
+		alert($(this).html());
+	});
+	$(".expendables").remove();
+	
+	// génération du DataTable
+	var table = $('.datatable').DataTable(
+		{
+			columnDefs: [{ targets: 'no-sort', orderable: false }],
+			order: [[ 1, "asc" ]],
+			bFilter: false,
+			bPaginate: false,
+    		bLengthChange: false,
+    		bInfo: false
+		}
+	);
+	
+	// activation / desactivation des tr en plus
+	$( "body" ).on( "click", ".expand", function() {	
+		var tr = $(this).closest('tr');
+		var row = table.row( tr );
+ 
+		if ( row.child.isShown() ) {
+			$(this).removeClass('glyphicon-minus-sign').addClass('glyphicon-plus-sign');
+			// This row is already open - close it
+			row.child.hide();
+			tr.removeClass('shown');
+		}
+		else {
+			// Open this row
+			$(this).removeClass('glyphicon-plus-sign').addClass('glyphicon-minus-sign');
+			row.child( myArray[$(this).data('name')] ).show();
+			tr.addClass('shown');
+		}
+	});
+
 });
-$( "body" ).on( "click", ".glyphicon-minus", function() {	
-	$(this).removeClass('glyphicon-minus').addClass('glyphicon-plus');
-	$(this).parent().parent().next('tr').addClass("hidden");
-});
+
+// Add event listener for opening and closing details
