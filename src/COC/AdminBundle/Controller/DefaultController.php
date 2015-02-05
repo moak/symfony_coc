c61ce9ccab8b2121a4798b23a8c74229f5cbf12a
Seyaa <barbot.max@gmail.com>
2015-02-05 21:13:49 +0100

    Fix admin with clan changes

--- src/COC/AdminBundle/Controller/DefaultController.php
+++ src/COC/AdminBundle/Controller/DefaultController.php
@@ -10,11 +10,11 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $numberPlayers = $em->getRepository('COCBundle:Player')->getNumberPlayers();
-        $numberBases = $em->getRepository('COCBundle:ImageBase')->getNumberBases();
-        $numberVideos = $em->getRepository('COCBundle:Video')->getNumberVideos();
+        $numberPlayers = $em->getRepository('COCBundle:Player')->getNumberEntities($id_clan);
+        $numberBases = $em->getRepository('COCBundle:ImageBase')->getNumberEntities($id_clan);
+        $numberVideos = $em->getRepository('COCBundle:Video')->getNumberEntities($id_clan);
         $numberUsersNonAssigned = $em->getRepository('ApplicationSonataUserBundle:User')->getNumberUsersNonAssigned();
-        $numberImagesBonus = $em->getRepository('COCBundle:ImageBonus')->getNumberImagesBonus();
+        $numberImagesBonus = $em->getRepository('COCBundle:ImageBonus')->getNumberEntities($id_clan);
         $clan = $this->container->get('coc_cocbundle.clan_info')->getClan($id_clan);
 
         return $this->render('AdminBundle:Default:index.html.twig' , array('clan' => $clan, 'numberImagesBonus'=> $numberImagesBonus,'numberPlayers'=> $numberPlayers,'numberUsersNonAssigned' => $numberUsersNonAssigned, 'numberBases'=> $numberBases, 'numberVideos' => $numberVideos));
@@ -25,13 +25,13 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $numberUsers = $em->getRepository('ApplicationSonataUserBundle:User')->getNumberUsers();
 
-        $numberImagesBonus = $em->getRepository('COCBundle:ImageBonus')->getNumberImagesBonus();
+        $numberImagesBonus = $em->getRepository('COCBundle:ImageBonus')->getNumberEntities($id_clan);
 
-        $numberPlayers = $em->getRepository('COCBundle:Player')->getNumberPlayers();
-        $numberBases = $em->getRepository('COCBundle:ImageBase')->getNumberBases();
-        $numberVideos = $em->getRepository('COCBundle:Video')->getNumberVideos();
-        $numberWars = $em->getRepository('COCBundle:War')->getNumberWars();
-        $numberBestAttacks = $em->getRepository('COCBundle:ImageBestAttack')->getNumberBestAttacks();
+        $numberPlayers = $em->getRepository('COCBundle:Player')->getNumberEntities($id_clan);
+        $numberBases = $em->getRepository('COCBundle:ImageBase')->getNumberEntities($id_clan);
+        $numberVideos = $em->getRepository('COCBundle:Video')->getNumberEntities($id_clan);
+        $numberWars = $em->getRepository('COCBundle:War')->getNumberEntities($id_clan);
+        $numberBestAttacks = $em->getRepository('COCBundle:ImageBestAttack')->getNumberEntities($id_clan);
         $numberUsersNonAssigned = $em->getRepository('ApplicationSonataUserBundle:User')->getNumberUsersNonAssigned();
         $clan = $this->container->get('coc_cocbundle.clan_info')->getClan($id_clan);
 
