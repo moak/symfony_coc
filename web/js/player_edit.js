c10cf21edc84f6a480e0575fc167b589bacf6b0d
Seyaa <barbot.max@gmail.com>
2015-02-26 21:20:48 +0100

    grrr

--- web/js/player_edit.js
+++ web/js/player_edit.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
 	var town_hall_level_max = 10;
 	var town_hall_level = $("#players_hall_town" ).val();
-	town_hall_level++;
 
-	for (i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
+	for (var i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
 		$.each($("[data-town-hall-level-min=" + i + "]"), function() {
 			if( town_hall_level >= i ) $("[data-town-hall-level-min=" + i + "]").show();
 			else $("[data-town-hall-level-min=" + i + "]").hide();
@@ -14,9 +13,8 @@ $( document ).ready(function() {
 $( "#players_hall_town" ).on( "change", function() {
 	var town_hall_level_max = 10;
 	var town_hall_level = $("#players_hall_town" ).val();
-	town_hall_level++;
-
-	for (i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
+    
+	for (var i = 1; i <= town_hall_level_max; i++) { // je boucle sur lui
 		$.each($("[data-town-hall-level-min=" + i + "]"), function() {
 			if( town_hall_level >= i ) $("[data-town-hall-level-min=" + i + "]").show();
 			else $("[data-town-hall-level-min=" + i + "]").hide();
