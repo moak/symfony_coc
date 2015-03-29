55763fddf000e521881f09691b55dc52b64da947
Seyaa <barbot.max@gmail.com>
2015-03-30 00:31:05 +0200

    update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,7 +39,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new FOS\CommentBundle\FOSCommentBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
-
+            new KPhoen\SmsSenderBundle\KPhoenSmsSenderBundle(),
 
         );
 
